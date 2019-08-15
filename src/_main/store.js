e40b32ee72df5de1567d9d3b05990d54136303d5
Aziz Ali <aziz@ilovecoding.org>
2019-08-15 11:44:32 -0700

    Apply Middleware Redux logger

--- src/_main/store.js
+++ src/_main/store.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+let store;
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  store = createStore(rootReducer);
+} else {
+  store = createStore(rootReducer, applyMiddleware(logger));
+}
 
 export default store;
